d03bc77e24d4543c3a27784310110504cd9fdfeb
ADMIN <mixingsteven@outlook.com>
2023-05-06 10:33:55 +0800

    init

---
+++ src/main/java/com/stevenhornghub/inventoryoftoys/InventoryOfToysApplication.java
@@ -0,0 +1,13 @@
+package com.stevenhornghub.inventoryoftoys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryOfToysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryOfToysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stevenhornghub/inventoryoftoys/InventoryOfToysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stevenhornghub.inventoryoftoys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryOfToysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
